f70c83e8319af67d8c37028dd8463d3ff57b7f95
Heechang Jeong <wjdgmlckd96@daum.net>
2022-07-08 16:27:32 +0900

    first commit

---
+++ src/component/Navbar.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUser } from '@fortawesome/free-regular-svg-icons';
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
+import { useNavigate} from "react-router-dom";
+import { Button } from "react-bootstrap";
+
+const Navbar = () => {
+    const menuList = [
+        '한식',
+        '중식',
+        '일식',
+        '야식',
+        '카페',
+    ];
+
+const navigate = useNavigate();
+
+const goToLogin = () => {
+    navigate('/login');
+};    
+    return (
+    <div>
+        <div>
+            <div className="login-button" onClick={goToLogin}>
+                <FontAwesomeIcon icon={faUser} />
+                <div>로그인</div>
+            </div>
+        </div>
+
+        <div className="nav-section">
+            <img 
+            width={100}
+            src="https://3.bp.blogspot.com/--g1ftdzaV8o/We8_zN8b_nI/AAAAAAAAAqs/VFXs_X7QkFECMZZkhNGudsYV6Ws1m2k7wCLcBGAs/s1600/gbgsLOGO%2528gbgs.go.kr%2529.gif"
+        
+            />
+        </div>
+        
+        <div className="menu-area">
+            <div className="input-area">
+                <FontAwesomeIcon icon={faSearch}/>
+                <input type="text"/>
+            </div>
+            
+            <ul className="menu-list">
+                {menuList.map(menu=>
+                <li>{menu}</li>
+                )}
+            </ul>
+
+            <div className="button-style">
+                 <Button>건의하기</Button>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
---
+++ src/page/Total.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+const Total = () => {
+  return (
+    <div>
+      <InfoCard/>
+    </div>
+  )
+}
+
+export default Total
